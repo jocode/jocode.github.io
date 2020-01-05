f5f579eb9ee93f65bbbf1527ded017dac6d6a6f0
Johan Mosquera <joncamilomoni@gmail.com>
2020-01-05 09:46:18 -0500

    Envío de correo
    Se  realiza la funcionalidad del formulario para enviar el correo electrónico de contacto en el sitio web

--- js/script.js
+++ js/script.js
@@ -1,6 +1,6 @@
-$(function(){
+$(function () {
 
-	$('a.page-scroll').on('click', function(event) {
+    $('a.page-scroll').on('click', function (event) {
         var element = $(this);
         var section = $(element.attr('href'));
         $('html, body').animate({
@@ -8,5 +8,44 @@ $(function(){
         }, 1000);
         event.preventDefault();
     });
+
+    var form = document.getElementById("formulario");
+    var result = document.getElementById("result");
+    var send = document.getElementById("send");
+
+    var name = form.nombre;
+    var email = form.email;
+    var message = form.mensaje;
+
+    form.addEventListener('submit', check);
+
+    function check(e) {
+        e.preventDefault();
+        $.post(
+            "https://jocode.000webhostapp.com/mail/me/",
+            {
+                "send": "send",
+                "name": name.value,
+                "email": email.value,
+                "message": message.value
+            },
+            function (response) {
+                console.log(response);
+                if (response.status) {
+                    result.classList.toggle('alert-primary');
+                    result.innerText = response.message;
+                    form.reset();
+                } else {
+                    result.classList.toggle('alert-danger');
+                    result.innerText = response.message;
+                }
+            },
+            'json'
+        );
+    }
+
+    function disabledButton() {
+        send.setAttribute('disabled', true);
+    }
 
 });

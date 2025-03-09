a2c8f6744e69bed4cf5318cb146b175c9b66028b
Johan Mosquera <joncamilomoni@gmail.com>
2025-03-09 14:39:48 -0500

    Se agrega el nombre de usuario

--- website/astro.config.mjs
+++ website/astro.config.mjs
@@ -3,5 +3,6 @@ import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
+    site: 'https://jocode.github.io/',
     base: '/', // if hosted at https://yourusername.github.io/
 });

a5ea3e0d0c3206edfbc950467e7904faa8dd55d0
Johan Mosquera <joncamilomoni@gmail.com>
2025-03-09 14:30:15 -0500

    Se agrega el base para Github Pages

--- website/astro.config.mjs
+++ website/astro.config.mjs
@@ -2,4 +2,6 @@
 import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+    base: '/', // if hosted at https://yourusername.github.io/
+});

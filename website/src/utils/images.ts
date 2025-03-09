996586e883d055007e81c3825108a265f4a26c20
Johan Mosquera <joncamilomoni@gmail.com>
2025-03-09 16:38:16 -0500

    images are compressed

--- website/src/utils/images.ts
+++ website/src/utils/images.ts
@@ -2,14 +2,14 @@
 import type { ImageMetadata } from 'astro';
 
 // Import project images
-import tomatetutiempo from '../assets/img/trabajos/tomatetutiempo.png';
-import best_coffee from '../assets/img/trabajos/best_coffee.png';
-import busticket from '../assets/img/trabajos/busticket.png';
-import offitec from '../assets/img/trabajos/offitec.png';
-import comite_ganaderos from '../assets/img/trabajos/comite_ganaderos.png';
-import portfolio from '../assets/img/trabajos/portfolio.png';
-import juego_memoria from '../assets/img/trabajos/juego_memoria.png';
-import tetris_game from '../assets/img/trabajos/tetris_game.png';
+import tomatetutiempo from '../assets/img/trabajos/tomatetutiempo.webp';
+import best_coffee from '../assets/img/trabajos/best_coffee.webp';
+import busticket from '../assets/img/trabajos/busticket.webp';
+import offitec from '../assets/img/trabajos/offitec.webp';
+import comite_ganaderos from '../assets/img/trabajos/comite_ganaderos.webp';
+import portfolio from '../assets/img/trabajos/portfolio.webp';
+import juego_memoria from '../assets/img/trabajos/juego_memoria.webp';
+import tetris_game from '../assets/img/trabajos/tetris_game.webp';
 
 // Define the valid project image keys
 export type ProjectImageKey = 'tomatetutiempo' | 'best_coffee' | 'busticket' | 'offitec' | 
